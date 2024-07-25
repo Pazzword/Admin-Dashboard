bbf49489e04b398d22310f614498e8dc959073bc
Anzor <anzor.pozner@gmail.com>
2024-07-25 20:12:27 +0100

    Dashboard JS and colors adjusted

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,95 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import api from "../api";
-import Note from "../components/Note";
-import "../styles/Home.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from "../components/Form"
 
 function Home() {
-    const [notes, setNotes] = useState([]);
-    const [content, setContent] = useState("");
-    const [title, setTitle] = useState("");
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        getNotes();
-    }, []);
-
-    const getNotes = () => {
-        api
-            .get("/api/notes/")
-            .then((res) => res.data)
-            .then((data) => {
-                setNotes(data);
-                console.log(data);
-            })
-            .catch((err) => alert(err));
-    };
-
-    const deleteNote = (id) => {
-        api
-            .delete(`/api/notes/delete/${id}/`)
-            .then((res) => {
-                if (res.status === 204) alert("Note deleted!");
-                else alert("Failed to delete note.");
-                getNotes();
-            })
-            .catch((error) => alert(error));
-    };
-
-    const createNote = (e) => {
-        e.preventDefault();
-        api
-            .post("/api/notes/", { content, title })
-            .then((res) => {
-                if (res.status === 201) alert("Note created!");
-                else alert("Failed to make note.");
-                getNotes();
-            })
-            .catch((err) => alert(err));
-    };
-
-
-
     return (
-        <div className="col-10">
-        <div className="home-container">
-            <div className="notes-section">
-                <h2>Let's talk</h2>
-                {notes.map((note) => (
-                    <Note note={note} onDelete={deleteNote} key={note.id} />
-                ))}
-            </div>
-            <div className="form-section">
-                <h2>Write your Review</h2>
-                <form onSubmit={createNote}>
-                    <label htmlFor="title">Title:</label>
-                    <br />
-                    <input
-                        type="text"
-                        id="title"
-                        name="title"
-                        required
-                        onChange={(e) => setTitle(e.target.value)}
-                        value={title}
-                    />
-                    <label htmlFor="content">Content:</label>
-                    <br />
-                    <textarea
-                        id="content"
-                        name="content"
-                        required
-                        value={content}
-                        onChange={(e) => setContent(e.target.value)}
-                    ></textarea>
-                    <br />
-                    <input type="submit" value="Submit" />
-                    <br />
-                </form>
-            </div>
-        </div>
-        </div>
-    );
+        <div>Anzor</div>
+    )
 }
 
-export default Home;
+export default Home
